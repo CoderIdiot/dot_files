c2b044bfd09f7e28987efd984bb9934eeae61d64
HaiChiang <rq8686@qq.com>
2018-05-22 11:27:25 +0800

    Update at: Tue May 22 11:27:25 CST 2018

--- in.sh
+++ in.sh
@@ -14,7 +14,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install Zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 chsh -s /bin/zsh $USER
 
